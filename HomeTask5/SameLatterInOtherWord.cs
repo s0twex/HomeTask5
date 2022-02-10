45f9824da1f1206127ad481d3f332ff564fd6244
hohmu <hohmu@DESKTOP-IK9JCN6>
2022-02-10 16:06:46 +0300

    Добавьте файлы проекта.

---
+++ HomeTask5/SameLatterInOtherWord.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameLatterInOtherWord
+{
+    class Program
+    {
+        static bool isThisPermutation(string s1, string s2)
+        {
+            return s1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(s2.Select(Char.ToUpper).OrderBy(x => x));
+        }
+
+        
+        static bool isThisPermutation2(string s1, string s2)
+        {
+            s1 = s1.ToLower();
+            s2 = s2.ToLower();
+
+            string news1 = s1[0].ToString();
+            string news2 = s2[0].ToString();
+
+            for (int i = 1; i < s1.Length; i++)
+                putCharIntoStr(ref news1, s1[i]);
+
+            for (int i = 1; i < s2.Length; i++)
+                putCharIntoStr(ref news2, s2[i]);
+
+            if (news1.Equals(news2))
+                return true;
+            else
+                return false;
+        }
+
+        
+        static void putCharIntoStr(ref string s, char ch)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] > ch)
+                {
+                    s = s.Insert(i, ch.ToString());
+                    break;
+                }
+                else
+                    if (i == s.Length - 1)
+                {
+                    s += ch.ToString();
+                    break;
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string first = "GeekBrains";
+            string second = "sniarBkeeG";
+
+            Console.WriteLine("Вас приветствует программа проверки является ли одна строка перестановкой другой.");
+
+            Console.WriteLine("Проверим следующие строки: " + first + ", и " + second);
+
+            if (isThisPermutation(first, second) == true && isThisPermutation2(first, second) == true)
+                Console.WriteLine("Строки являются перестановками друг друга.");
+            else
+                Console.WriteLine("Строки состоят из разных символов.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Message/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа демонстрации возможностей статического класса Message.");
+
+            Console.WriteLine("\nИмеется следующий текст: \n" + Message.text);
+
+            Console.WriteLine("\nВыведем слова текста, которые содержат не более 5 букв:");
+            Message.GetWordsByLength(5);
+
+            Console.WriteLine();
+            Console.Write("\nУдалим из текста слова, заканчивающиеся на 'я': ");
+            Message.DeleteWordByEndChar('я');
+
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+
+
+            Console.WriteLine("\nСформированная строка StringBuilder из самых длинных слов сообщения: \n" + Message.GetLongWordsString());
+
+            Console.WriteLine("\nПроизведём частотный анализ текста: ");
+            string[] arr = { "шел", "его", "тело", "жив", "он" };
+            Message.FrequencyAnalysis(arr, Message.text);
+
+            Console.ReadKey();
+        }
+    }
+}
